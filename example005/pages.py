0a560009d31fa03d1d071257f950c96f207a1d32
Oleg Mikheenko <o.mikheenko@mail.ru>
2019-08-16 17:45:18 +0300

    added three tests to example005 with page object

--- example005/pages.py
+++ example005/pages.py
@@ -1,10 +1,56 @@
 from page_objects import PageObject, PageElement, MultiPageElement
-
+from time import sleep
+from selenium.webdriver import ActionChains
 
 class ProskaterMainPage(PageObject):
 
-    search_text = PageElement(xpath='//*[@name="keywordss"]')
+    search_text = PageElement(name='keywords')
+    search_button = PageElement(xpath='(//button[@type="submit"])[1]')
+    page_title = PageElement(xpath='(//a)[1]')
 
     def __init__(self, web_driver, uri=''):
         super().__init__(web_driver, uri)
         self.get('https://www.proskater.ru/')
+
+
+    def search(self, text):
+        self.search_text = text
+        self.search_button.click()
+        return SearchResultsPage(self.w)
+
+    def open_section(self, text):
+        search_button = self.w.find_element_by_xpath('//a[text()="' + text + '"]')
+        search_button.click()
+        return FilterPage(self.w)
+
+class SearchResultsPage(PageObject):
+
+    search_results = MultiPageElement(xpath='//div[@class="photo"]')
+    popup_close = PageElement(xpath='(//a[@class="btn-close1"])[9]')
+    displayed_results_number = PageElement(xpath='//*[@class="listing-progress-text__viewed-count"]')
+    all_results_number = PageElement(xpath='//*[@class="listing-progress-text__all-count"]')
+    no_results_found = PageElement(xpath='(//div[@class="no-products"])[2]')
+    first_result = PageElement(xpath='(//div[@class="photo"])[1]')
+    add_to_cart_button = PageElement(xpath='(//div[@class="add-cart"])[1]') # first result "Add to cart" button
+
+    def __init__(self, web_driver, uri=''):
+        super().__init__(web_driver, uri)
+        self.popup_close.click()
+        sleep(2)
+
+    def add_to_cart(self):
+        sleep(5)
+        action = ActionChains(self.w)
+        # action.move_to_element(self.first_result).pause(5).perform()
+        #sleep(10)
+        self.add_to_cart_button.click()
+
+class FilterPage(PageObject):
+
+    section_title = PageElement(xpath='//*[@class="inlineBlock"]')
+    popup_close = PageElement(xpath='(//a[@class="btn-close1"])[10]')
+
+    def __init__(self, web_driver, uri=''):
+        super().__init__(web_driver, uri)
+        self.popup_close.click()
+        sleep(5)
--- example005/test_selenium.py
+++ example005/test_selenium.py
@@ -1,8 +1,43 @@
 from time import sleep
 from pages import ProskaterMainPage
+import pytest
+# python3 -m pytest -v -s --driver Chrome --driver-path ../driver/chromedriver -m run test_selenium.p
 
-def test_google(web_browser):
+def test_search(web_browser):
     page = ProskaterMainPage(web_browser)
 
-    page.search_text = 'super kicks'
-    sleep(10)
+    search_page = page.search('кеды')
+    sleep(5)
+    assert len(search_page.search_results) == 40
+    assert int(search_page.displayed_results_number.text) == 40
+    assert int(search_page.all_results_number.text) >= 40
+
+
+def test_search_no_results(web_browser):
+    page = ProskaterMainPage(web_browser)
+    search_page = page.search('несуществующий товар')
+    assert search_page.no_results_found.text == 'НИЧЕГО НЕ НАШЛОСЬ\nПОПРОБУЙТЕ ИЗМЕНИТЬ ЗАПРОС:'
+    assert search_page.no_results_found.is_displayed() == True
+
+
+
+def test_open_men_section(web_browser):
+    page = ProskaterMainPage(web_browser)
+    filter_page = page.open_section('мужское')
+    assert filter_page.section_title.text == 'МУЖСКАЯ КОЛЛЕКЦИЯ'
+    sleep(2)
+
+
+@pytest.mark.run
+def test_add_to_chart(web_browser):
+    page = ProskaterMainPage(web_browser)
+    search_page = page.search('кеды')
+    search_page.add_to_cart()
+    sleep(5)
+
+def delete_from_chart(web_browser):
+    pass
+
+
+def sort_by_price(web_browser):
+    pass
